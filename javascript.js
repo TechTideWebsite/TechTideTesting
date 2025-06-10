8d093e18da495a1b9508ce76d3930e144fa74029
Rad-Coding <pjobriengamez@gmail.com>
2025-06-10 13:38:27 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,11 +1,7 @@
-  window.onload = function() {
+window.addEventListener("load", function() {
     let screenWidth = window.innerWidth / window.devicePixelRatio;
-
     let zoomLevel = screenWidth >= 2500 ? 1 : (screenWidth / 2560) * 0.848;
-
     zoomLevel = Math.min(zoomLevel, 1);
-
     document.body.style.zoom = (zoomLevel * 100) + "%";
-
     console.log("Detected Width: " + screenWidth + "px, Applied Zoom: " + document.body.style.zoom);
-  };
+});
